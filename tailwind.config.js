c2cfa0967e95d699e1510f19effbd3191059094e
Abubakar Malik <abub81159@gmail.com>
2024-11-29 12:32:58 +0500

    Update package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "./components/**/*.{js,jsx}",
     "./app/**/*.{js,jsx}",
     "./src/**/*.{js,jsx}",
+    'node_modules/@radix-ui/**/*.{js,ts,jsx,tsx}',
   ],
   prefix: "",
   theme: {
